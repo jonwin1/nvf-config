01c01934b32744cd3abfd40e65af3cc9f0b0f124
Jonatan Wincent <jonatan.wincent@proton.me>
2026-02-07 11:29:34 +0100

    visual

---
+++ config/visual.nix
@@ -0,0 +1,16 @@
+{
+  vim = {
+    theme = {
+      enable = true;
+      name = "gruvbox";
+      style = "dark";
+      transparent = true;
+    };
+
+    visuals = {
+      nvim-web-devicons.enable = true;
+    };
+
+    ui.borders.enable = true;
+  };
+}
