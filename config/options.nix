785d876ed2c5512617f6510425c6050116555878
Jonatan Wincent <jonatan.wincent@proton.me>
2026-02-07 11:29:09 +0100

    options

---
+++ config/options.nix
@@ -0,0 +1,23 @@
+{
+  vim = {
+    clipboard = {
+      enable = true;
+      registers = "unnamedplus";
+    };
+    undoFile.enable = true;
+    searchCase = "smart";
+
+    options = {
+      mouse = "";
+
+      tabstop = 4;
+      shiftwidth = 0;
+      smartindent = true;
+
+      scrolloff = 4;
+      sidescrolloff = 4;
+
+      wrap = false;
+    };
+  };
+}
