d6b8996792ede1bb752dc18c840d0613dca6a9e0
Jonatan Wincent <jonatan.wincent@proton.me>
2026-08-29 21:47:22 +0200

    nvf.nixpkgs follows nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nvf.url = "github:notashelf/nvf";
+    nvf = {
+      url = "github:NotAShelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     neowiki = {
       url = "github:echaya/neowiki.nvim";
