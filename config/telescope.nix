abed000b486b8bccb20927a5ff42966fe823f390
Jonatan Wincent <jonatan.wincent@proton.me>
2026-09-17 09:19:51 +0200

    vertical telescope layout

--- config/telescope.nix
+++ config/telescope.nix
@@ -1,6 +1,15 @@
 {
   vim.telescope = {
     enable = true;
+
+    setupOpts.defaults = {
+      layout_strategy = "vertical";
+      layout_config = {
+        vertical.mirror = true;
+        preview_cutoff = 0;
+      };
+    };
+
     mappings = {
       open = "<leader>fo";
       treesitter = null;
